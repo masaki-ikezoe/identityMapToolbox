21ee77da687933b4df5d9372bd7a01a600ff9d13
masaki-ikezoe <masaki-ikezoe@users.noreply.github.com>
2016-01-22 16:19:39 +0900

    add new file

---
+++ createconditions.m
@@ -0,0 +1,17 @@
+clear; close all; clc;
+
+%% Select File
+[FileName,PathName] = uigetfile('MultiSelect', 'off');
+
+%% Create conditions
+% Load Stimulus 
+load([PathName FileName]);
+% Create names
+names = createnames(expConditions);
+% Create sots
+onsets = createonsets(expConditions);
+% Create durations
+durations = createdurations(expConditions, 0);
+
+%% Sove Conditions
+save([PathName 'conditions.mat'], 'names', 'onsets', 'durations');
